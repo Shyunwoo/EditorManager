2d0dedf9527a1c6edac4bac31ea799b8bf8ed514
Shyunwoo219gmail.com <90670046+Shyunwoo@users.noreply.github.com>
2022-12-30 16:23:22 +0900

    Create Editor Manager

---
+++ Plugins/SuperManager/Source/SuperManager/Private/AssetActions/QuickAssetAction.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AssetActions/QuickAssetAction.h"
+#include "DebugHeader.h"
+#include "EditorUtilityLibrary.h"
+#include "EditorAssetLibrary.h"
+#include "Misc/MessageDialog.h"
+
+void UQuickAssetAction::DuplicateAssets(int32 NumOfDuplicates)
+{
+	if (NumOfDuplicates <= 0)
+	{
+		ShowMsgDialog(EAppMsgType::Ok, TEXT("Please enter a valid number"));
+		return;
+	}
+
+	TArray<FAssetData> SelectedAssetsData = UEditorUtilityLibrary::GetSelectedAssetData();
+	uint32 Counter = 0;
+
+	for (const FAssetData& SelectedAssetData : SelectedAssetsData)
+	{
+		for (int32 i = 0; i < NumOfDuplicates; i++)
+		{
+			const FString SourceAssetPath = SelectedAssetData.ObjectPath.ToString();
+			const FString NewDuplicatedAssetName = SelectedAssetData.AssetName.ToString() + TEXT("_") + FString::FromInt(i+1);
+			const FString NewPathName = FPaths::Combine(SelectedAssetData.PackagePath.ToString(), NewDuplicatedAssetName);
+
+			if (UEditorAssetLibrary::DuplicateAsset(SourceAssetPath, NewPathName))
+			{
+				UEditorAssetLibrary::SaveAsset(NewPathName, false);
+				++Counter;
+			}
+		}
+	}
+
+	if (Counter > 0)
+	{
+		ShowNotifyInfo(TEXT("Successfully duplicated " + FString::FromInt(Counter) + " files"));
+	}
+}
